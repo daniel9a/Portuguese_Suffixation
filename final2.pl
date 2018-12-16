20c341e987bb9562ebc042c316c8fb0449efc339
Daniel <danandrade@ucdavis.edu>
2018-12-15 16:00:44 -0800

    added files

---
+++ final2.pl
@@ -0,0 +1,238 @@
+% DETERMINERS
+noun([e,u], [eu, singular, determiner]).
+noun([t,u], [tu, singular, determiner]).
+noun([e,l,e], [ele, singular, determiner]).
+noun([e,l,a], [ela, singular, determiner]).
+noun([n,ó,s], [nós, plural, determiner]).
+noun([v,ó,s], [vós, plural, determiner]).
+noun([e,l,e,s], [eles, plural, determiner]).
+noun([e,l,a,s], [elas, plural, determiner]).
+
+verb([c,a,n,t,a,r], [cantar, regular]).
+verb([s,e,r], [ser, irregular]).
+
+% CANTAR RULES
+verb([o], [cantar, singular, first, present, eu]).
+verb([a,s], [cantar, singular, second, present, tu]).
+verb([a], [cantar, singular, third, present, ele]).
+verb([a], [cantar, singular, third, present, ela]).
+verb([a,m,o,s], [cantar, plural, first, present, nós]).
+verb([a,i,s], [cantar, plural, second, present, vós]).
+verb([a,m], [cantar, plural, third, present, eles]).
+verb([a,m], [cantar, plural, third, present, elas]).
+
+verb([e,i], [cantar, singular, first, preterite, eu]).
+verb([a,s,t,e], [cantar, singular, second, preterite, tu]).
+verb([o,u], [cantar, singular, third, preterite, ele]).
+verb([o,u], [cantar, singular, third, preterite, ela]).
+verb([a,m,o,s], [cantar, plural, first, preterite, nós]).
+verb([a,s,t,e,s], [cantar, plural, second, preterite, vós]).
+verb([a,r,a,m], [cantar, plural, third, preterite, eles]).
+verb([a,r,a,m], [cantar, plural, third, preterite, elas]).
+
+verb([a,v,a], [cantar, singular, first, imperfect, eu]).
+verb([a,v,a,s], [cantar, singular, second, imperfect, tu]).
+verb([a,v,a], [cantar, singular, third, imperfect, ele]).
+verb([a,v,a], [cantar, singular, third, imperfect, ela]).
+verb([á,v,a,m,o,s], [cantar, plural, first, imperfect, nós]).
+verb([á,v,e,i,s], [cantar, plural, second, imperfect, vós]).
+verb([a,v,a,m], [cantar, plural, third, imperfect, eles]).
+verb([a,v,a,m], [cantar, plural, third, imperfect, elas]).
+
+verb([a,r,a], [cantar, singular, first, pluperfect, eu]).
+verb([a,r,a,s], [cantar, singular, second, pluperfect, tu]).
+verb([a,r,a], [cantar, singular, third, pluperfect, ele]).
+verb([a,r,a], [cantar, singular, third, pluperfect, ela]).
+verb([á,r,a,m,o,s], [cantar, plural, first, pluperfect, nós]).
+verb([á,r,e,i,s], [cantar, plural, second, pluperfect, vós]).
+verb([a,r,a,m], [cantar, plural, third, pluperfect, eles]).
+verb([a,r,a,m], [cantar, plural, third, pluperfect, elas]).
+
+verb([a,r,e,i], [cantar, singular, first, future, eu]).
+verb([a,r,á,s], [cantar, singular, second, future, tu]).
+verb([a,r,á], [cantar, singular, third, future, ele]).
+verb([a,r,á], [cantar, singular, third, future, ela]).
+verb([á,r,e,m,o,s], [cantar, plural, first, future, nós]).
+verb([a,r,e,i,s], [cantar, plural, second, future, vós]).
+verb([a,r,ã,o], [cantar, plural, third, future, eles]).
+verb([a,r,ã,o], [cantar, plural, third, future, elas]).
+
+% COMER RULES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+verb([o], [comer, singular, first, present, eu]).
+verb([e,s], [comer, singular, second, present, tu]).
+verb([e], [comer, singular, third, present, ele]).
+verb([e], [comer, singular, third, present, ela]).
+verb([e,m,o,s], [comer, plural, first, present, nós]).
+verb([e,i,s], [comer, plural, second, present, vós]).
+verb([e,m], [comer, plural, third, present, eles]).
+verb([e,m], [comer, plural, third, present, elas]).
+
+verb([i], [comer, singular, first, preterite, eu]).
+verb([e,s,t,e], [comer, singular, second, preterite, tu]).
+verb([e,u], [comer, singular, third, preterite, ele]).
+verb([e,u], [comer, singular, third, preterite, ela]).
+verb([e,m,o,s], [comer, plural, first, preterite, nós]).
+verb([e,s,t,e,s], [comer, plural, second, preterite, vós]).
+verb([e,r,a,m], [comer, plural, third, preterite, eles]).
+verb([e,r,a,m], [comer, plural, third, preterite, elas]).
+
+verb([i,a], [comer, singular, first, imperfect, eu]).
+verb([i,a,s], [comer, singular, second, imperfect, tu]).
+verb([i,a], [comer, singular, third, imperfect, ele]).
+verb([i,a], [comer, singular, third, imperfect, ela]).
+verb([í,a,m,o,s], [comer, plural, first, imperfect, nós]).
+verb([í,e,i,s], [comer, plural, second, imperfect, vós]).
+verb([i,a,m], [comer, plural, third, imperfect, eles]).
+verb([i,a,m], [comer, plural, third, imperfect, elas]).
+
+verb([e,r,a], [comer, singular, first, pluperfect, eu]).
+verb([e,r,a,s], [comer, singular, second, pluperfect, tu]).
+verb([e,r,a], [comer, singular, third, pluperfect, ele]).
+verb([a,r,a], [comer, singular, third, pluperfect, ela]).
+verb([ê,r,a,m,o,s], [comer, plural, first, pluperfect, nós]).
+verb([ê,r,e,i,s], [comer, plural, second, pluperfect, vós]).
+verb([e,r,a,m], [comer, plural, third, pluperfect, eles]).
+verb([e,r,a,m], [comer, plural, third, pluperfect, elas]).
+
+verb([e,i], [comer, singular, first, future, eu]).
+verb([á,s], [comer, singular, second, future, tu]).
+verb([á], [comer, singular, third, future, ele]).
+verb([á], [comer, singular, third, future, ela]).
+verb([e,m,o,s], [comer, plural, first, future, nós]).
+verb([e,i,s], [comer, plural, second, future, vós]).
+verb([ã,o], [comer, plural, third, future, eles]).
+verb([ã,o], [comer, plural, third, future, elas]).
+
+% SOU RULES
+verb([s,o,u], [ser, singular, first, present, eu]).
+verb([é,s], [ser, singular, second, present, tu]).
+verb([é], [ser, singular, third, present, ele]).
+verb([é], [ser, singular, third, present, ela]).
+verb([s,o,m,o,s], [ser, plural, first, present, nós]).
+verb([s,o,i,s], [ser, plural, second, present, vós]).
+verb([s,ã,o], [ser, plural, third, present, eles]).
+verb([s,ã,o], [ser, plural, third, present, elas]).
+
+verb([f,u,i], [ser, singular, first, preterite, eu]).
+verb([f,o,s,t,e], [ser, singular, second, preterite, tu]).
+verb([f,o,i], [ser, singular, third, preterite, ele]).
+verb([f,o,i], [ser, singular, third, preterite, ela]).
+verb([f,o,m,o,s], [ser, plural, first, preterite, nós]).
+verb([f,o,s,t,e,s], [ser, plural, second, preterite, vós]).
+verb([f,o,r,a,m], [ser, plural, third, preterite, eles]).
+verb([f,o,r,a,m], [ser, plural, third, preterite, elas]).
+
+verb([e,r,a], [ser, singular, first, imperfect, eu]).
+verb([e,r,a,s], [ser, singular, second, imperfect, tu]).
+verb([e,r,a], [ser, singular, third, imperfect, ele]).
+verb([e,r,a], [ser, singular, third, imperfect, ela]).
+verb([é,r,a,m,o,s], [ser, plural, first, imperfect, nós]).
+verb([é,r,e,i,s], [ser, plural, second, imperfect, vós]).
+verb([e,r,a,m], [ser, plural, third, imperfect, eles]).
+verb([e,r,a,m], [ser, plural, third, imperfect, elas]).
+
+verb([f,o,r,a], [ser, singular, first, pluperfect, eu]).
+verb([f,o,r,a,s], [ser, singular, second, pluperfect, tu]).
+verb([f,o,r,a], [ser, singular, third, pluperfect, ele]).
+verb([f,o,r,a], [ser, singular, third, pluperfect, ela]).
+verb([f,ô,r,a,m,o,s], [ser, plural, first, pluperfect, nós]).
+verb([á,r,e,i,s], [ser, plural, second, pluperfect, vós]).
+verb([a,r,a,m], [ser, plural, third, pluperfect, eles]).
+verb([a,r,a,m], [ser, plural, third, pluperfect, elas]).
+
+verb([s,e,r,e,i], [ser, singular, first, future, eu]).
+verb([s,e,r,á,s], [ser, singular, second, future, tu]).
+verb([s,e,r,á], [ser, singular, third, future, ele]).
+verb([s,e,r,á], [ser, singular, third, future, ela]).
+verb([s,e,r,e,m,o,s], [ser, plural, first, future, nós]).
+verb([s,e,r,e,i,s], [ser, plural, second, future, vós]).
+verb([s,e,r,ã,o], [ser, plural, third, future, eles]).
+verb([s,e,r,ã,o], [ser, plural, third, future, elas]).
+
+% ESTAR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+verb([e,s,t,o,u], [estar, singular, first, present, eu]).
+verb([e,s,t,á,s], [estar, singular, second, present, tu]).
+verb([e,s,t,á], [estar, singular, third, present, ele]).
+verb([e,s,t,á], [estar, singular, third, present, ela]).
+verb([e,s,t,a,m,o,s], [estar, plural, first, present, nós]).
+verb([e,s,t,a,i,s], [estar, plural, second, present, vós]).
+verb([e,s,t,ã,o], [estar, plural, third, present, eles]).
+verb([e,s,t,ã,o], [estar, plural, third, present, elas]).
+
+verb([e,s,t,i,v,e], [estar, singular, first, preterite, eu]).
+verb([e,s,t,i,v,e,s,t,e], [estar, singular, second, preterite, tu]).
+verb([e,s,t,e,v,e], [estar, singular, third, preterite, ele]).
+verb([e,s,t,e,v,e], [estar, singular, third, preterite, ela]).
+verb([e,s,t,i,v,e,m,o,s], [estar, plural, first, preterite, nós]).
+verb([e,s,t,i,v,e,s,t,e,s], [estar, plural, second, preterite, vós]).
+verb([e,s,t,i,v,e,r,a,m], [estar, plural, third, preterite, eles]).
+verb([e,s,t,i,v,e,r,a,m], [estar, plural, third, preterite, elas]).
+
+verb([e,s,t,a,v,a], [estar, singular, first, imperfect, eu]).
+verb([e,s,t,a,v,a,s], [estar, singular, second, imperfect, tu]).
+verb([e,s,t,a,v,a], [estar, singular, third, imperfect, ele]).
+verb([e,s,t,a,v,a], [estar, singular, third, imperfect, ela]).
+verb([e,s,t,á,v,a,m,o,s], [estar, plural, first, imperfect, nós]).
+verb([e,s,t,á,v,e,i,s], [estar, plural, second, imperfect, vós]).
+verb([e,s,t,a,v,a,m], [estar, plural, third, imperfect, eles]).
+verb([e,s,t,a,v,a,m], [estar, plural, third, imperfect, elas]).
+
+verb([e,s,t,i,v,e,r,a], [estar, singular, first, pluperfect, eu]).
+verb([e,s,t,i,v,e,r,a,s], [estar, singular, second, pluperfect, tu]).
+verb([e,s,t,i,v,e,r,a], [estar, singular, third, pluperfect, ele]).
+verb([e,s,t,i,v,e,r,a], [estar, singular, third, pluperfect, ela]).
+verb([e,s,t,i,v,é,r,a,m,o,s], [estar, plural, first, pluperfect, nós]).
+verb([e,s,t,i,v,é,r,e,i,s], [estar, plural, second, pluperfect, vós]).
+verb([e,s,t,i,v,e,r,a,m], [estar, plural, third, pluperfect, eles]).
+verb([e,s,t,i,v,e,r,a,m], [estar, plural, third, pluperfect, elas]).
+
+verb([e,s,t,a,r,e,i], [estar, singular, first, future, eu]).
+verb([e,s,t,a,r,á,s], [estar, singular, second, future, tu]).
+verb([e,s,t,a,r,á], [estar, singular, third, future, ele]).
+verb([e,s,t,a,r,á], [estar, singular, third, future, ela]).
+verb([e,s,t,a,r,e,m,o,s], [estar, plural, first, future, nós]).
+verb([e,s,t,a,r,e,i,s], [estar, plural, second, future, vós]).
+verb([e,s,t,a,r,ã,o], [estar, plural, third, future, eles]).
+verb([e,s,t,a,r,ã,o], [estar, plural, third, future, elas]).
+
+% FUNCTION TO ALTER A IRREGULAR VERB
+% REPLACE VERB WITH PROPER IRREGULAR VERB FORM BASED OFF OF CONJUGATION
+change2(Number, Person, Mood, Verb, Determiner, Return) :-
+    verb(End, [Verb, Number, Person, Mood, Determiner]),
+    string_chars(Return, End).
+
+
+
+% FUNCTION TO ALTER A REGULAR VERB
+% REMOVE THE LAST TWO LETTERS AND APPEND PROPER FORM BASED OFF OF CONJUGATION
+change(Verblist, Number, Person, Mood, Verb, Determiner, Return) :-
+    reverse(Verblist, [_,_|Temp]),
+    reverse(Temp, Root),
+    verb(End, [Verb, Number, Person, Mood, Determiner]),
+    append(Root, End, R),
+    string_chars(Return, R).
+
+% CHECKS TO FIND OUT IF VERB IS IRREGULAR 
+% USE ATTRIBUTES LIKE DETERMINER, TYPE, NUMBER, PERSON, AND MOOD
+% RETURN ANSWER IN THE RETURN VARIABLE
+portuguese(Verb, Determiner, Type, Number, Person, Mood, Return) :-
+    noun(Noun, [Determiner, Number, determiner]),
+    verb(_, [Verb, irregular]),
+    Type = irregular,
+    change2(Number, Person, Mood, Verb, Determiner, X),
+    string_chars(StrN, Noun),
+    atom_concat(StrN, ' ', Temp),
+    atom_concat(Temp, X, Return).
+
+% CHECKS TO FIND OUT IF VERB IS REGULAR 
+% USE ATTRIBUTES LIKE DETERMINER, TYPE, NUMBER, PERSON, AND MOOD
+% RETURN ANSWER IN THE RETURN VARIABLE
+portuguese(Verb, Determiner, Type, Number, Person, Mood, Return) :-
+    noun(Noun, [Determiner, Number, determiner]),
+    verb(Verblist, [Verb, regular]),
+    Type = regular,
+    change(Verblist, Number, Person, Mood, Verb, Determiner, X),
+    string_chars(StrN, Noun),
+    atom_concat(StrN, ' ', Temp),
+    atom_concat(Temp, X, Return).
